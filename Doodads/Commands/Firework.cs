6afa7267f86a54b4ed096b75483048028d684d9b
DogooFalchion <Dogoo@nyxstudios.moe>
2016-10-29 12:31:06 -0400

    Initial commit for the Gizmos solution and Doodads project files.

---
+++ Doodads/Commands/Firework.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using Terraria;
+using TShockAPI;
+
+namespace Doodads.Commands
+{
+	class Firework : ICommandHandler
+	{
+		private readonly Command _command;
+
+		public Firework()
+		{
+			_command = new Command(Permissions.annoy, HandleCommand, "firework")
+			{
+				HelpText = "Spawns fireworks at a player."
+			};
+		}
+
+		public void RegisterCommand()
+		{
+			TShockAPI.Commands.ChatCommands.Add(_command);
+		}
+
+		public void HandleCommand(CommandArgs args)
+		{
+			if (args.Parameters.Count < 1)
+			{
+				args.Player.SendErrorMessage("Invalid syntax! Proper syntax: {0}firework <player> [red|green|blue|yellow]", TShockAPI.Commands.Specifier);
+				return;
+			}
+
+			var players = TShock.Utils.FindPlayer(args.Parameters[0]);
+			if (players.Count == 0)
+				args.Player.SendErrorMessage("Invalid player!");
+			else if (players.Count > 1)
+				TShock.Utils.SendMultipleMatchError(args.Player, players.Select(p => p.Name));
+			else
+			{
+				int type = 167;
+				if (args.Parameters.Count > 1)
+				{
+					if (args.Parameters[1].ToLower() == "green")
+						type = 168;
+					else if (args.Parameters[1].ToLower() == "blue")
+						type = 169;
+					else if (args.Parameters[1].ToLower() == "yellow")
+						type = 170;
+				}
+
+				var ply = players[0];
+				int p = Projectile.NewProjectile(ply.TPlayer.position.X, ply.TPlayer.position.Y - 64f, 0f, -8f, type, 0, (float)0);
+				Main.projectile[p].Kill();
+				args.Player.SendSuccessMessage("Launched Firework on {0}.", ply.Name);
+			}
+		}
+
+		public void DeregisterCommand()
+		{
+			TShockAPI.Commands.ChatCommands.Remove(_command);
+		}
+	}
+}
---
+++ Doodads/DoodadsPlugin.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using Doodads.Commands;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+
+namespace Doodads
+{
+	/// <summary>
+	/// The main plugin class should always be decorated with an ApiVersion attribute. The current API Version is 1.22
+	/// </summary>
+	[ApiVersion(1, 22)]
+	public class DoodadsPlugin : TerrariaPlugin
+	{
+		/// <summary>
+		/// The name of the plugin.
+		/// </summary>
+		public override string Name => "TShock Doodad Commands";
+
+		/// <summary>
+		/// The version of the plugin in its current state.
+		/// </summary>
+		public override Version Version => new Version(0, 1, 0);
+
+		/// <summary>
+		/// The author(s) of the plugin.
+		/// </summary>
+		public override string Author => "NyxStudios and Contributors";
+
+		/// <summary>
+		/// A short, one-line, description of the plugin's purpose.
+		/// </summary>
+		public override string Description => "Fun commands that have been removed from the core TShock plugin.";
+
+		/// <summary>
+		/// A List of command handlers for this Gizmo.
+		/// </summary>
+		private List<ICommandHandler> _commandHandlers = new List<ICommandHandler>();
+
+		/// <summary>
+		/// The plugin's constructor
+		/// Set your plugin's order (optional) and any other constructor logic here
+		/// </summary>
+		public DoodadsPlugin(Main game) : base(game)
+		{
+			_commandHandlers.Add(new Firework());
+		}
+
+		/// <summary>
+		/// Performs plugin initialization logic.
+		/// Add your hooks, config file read/writes, etc here
+		/// </summary>
+		public override void Initialize()
+		{
+			TShock.Initialized += InitializeCommands;
+		}
+
+		/// <summary>
+		/// Initialize all the commands in this gizmo.
+		/// </summary>
+		public void InitializeCommands()
+		{
+			TShock.Initialized -= InitializeCommands;
+			foreach (var command in _commandHandlers)
+			{
+				command.RegisterCommand();
+			}
+		}
+
+		/// <summary>
+		/// Performs plugin cleanup logic
+		/// Remove your hooks and perform general cleanup here
+		/// </summary>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				foreach (var command in _commandHandlers)
+				{
+					command.DeregisterCommand();
+				}
+				_commandHandlers = null;
+			}
+			base.Dispose(disposing);
+		}
+	}
+}
